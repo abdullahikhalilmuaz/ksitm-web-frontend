1c6b26c9ad378f881e4d7bdb80908d6b87f0bd8e
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-09-05 18:08:11 +0100

    routing issue solution

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "60px 20px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+      <Link href="/" style={{ color: "#4B2E83", fontWeight: 600 }}>
+        Go back home
+      </Link>
+    </div>
+  );
+}
