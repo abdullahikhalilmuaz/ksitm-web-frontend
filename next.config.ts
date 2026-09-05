8c1bb4568ec2ae3499c84ac659958b8ba32cebb5
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-09-05 20:14:30 +0100

    vercel routing issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    domains: [
+      "localhost",
+      "ksitm-backend-api.onrender.com", // Your backend
+      "ksitm-library.vercel.app", // Your Vercel domain
+    ],
+  },
 };
 
 export default nextConfig;
